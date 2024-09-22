be13d698ba8cb3558b5fda6e37aeb7751bc57c1b
SOUFIANEOULAHCEN <soufiane.oulahcen@gmail.com>
2024-09-22 22:30:40 +0100

    section home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+const { Container } = require("postcss");
+
 module.exports = {
   content: [
     './src/**/*.html',
     './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: { // Utilise "container" ici sans majuscule
+        center: true, // Centre automatiquement le conteneur
+        padding: '2rem', // Ajoute du padding à tous les côtés du conteneur
+        screens: { // Définit des tailles de conteneurs pour différentes tailles d'écran
+          sm: '100%',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+        },
+      },
+    },
   },
   plugins: [],
 }
